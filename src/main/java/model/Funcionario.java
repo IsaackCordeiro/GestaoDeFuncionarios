1505c1649fcdbb9a50c26b5da36605426fcf1fe8
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-10-28 21:33:48 -0300

    alterado class funcionario
    adicionado atributo salariobase , construtor e getter e setter para todos os atributos

--- src/main/java/model/Funcionario.java
+++ src/main/java/model/Funcionario.java
@@ -11,6 +11,35 @@ package model;
 public class Funcionario {
     private String nome ;
     private String cargo;
+    private double salarioBase;
     
-    
+    public Funcionario(String nome, String cargo, double salarioBase) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }  
 }
