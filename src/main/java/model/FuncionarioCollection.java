4fe028aed87a0b3343b8fd2a7d15104fc268a1d1
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-10-28 23:35:25 -0300

    alteracao funcionarioCollection
    logica para CRUD + add de test

--- src/main/java/model/FuncionarioCollection.java
+++ src/main/java/model/FuncionarioCollection.java
@@ -3,11 +3,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
+import java.util.ArrayList;
 
 /**
  *
  * @author isaac
  */
 public class FuncionarioCollection {
-    
+ private ArrayList<Funcionario> colaboradores= new ArrayList<>();;
+
+
+ 
+ ArrayList<Funcionario> pesquisaFuncionario(String nomefuncionario){
+     ArrayList<Funcionario> funcionariobuscado = new ArrayList<Funcionario>();
+     for (Funcionario func : colaboradores) {
+         if(nomefuncionario.equals(func.getNome())){
+             funcionariobuscado.add(func);
+         }
+     }
+      return funcionariobuscado;
+ }
+ 
+ void adicionarFuncionario(Funcionario funcionario){
+     colaboradores.add(funcionario);
+ }
+
+ void editarFuncionario(int index,Funcionario func){
+     colaboradores.set(index, func);
+ }
+ 
+ void removerFuncionario(int index){
+     colaboradores.remove(index);
+ }
+ 
+ 
+ public ArrayList<Funcionario> getColaboradores() {
+        return colaboradores;
+    }
+void pFuncionario(){
+     ArrayList<Funcionario> funcionariobuscado = new ArrayList<Funcionario>();
+     for (Funcionario func : colaboradores) {
+         System.out.println(func.getNome());
+     }
+     
+ }
 }
---
+++ src/main/java/model/test.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author dougl
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Testando");
+        //
+        Funcionario f1 = new Funcionario("douglas","estágiario",2000);
+        Funcionario f2 = new Funcionario("thiago","CTO",200000);
+        Funcionario f3 = new Funcionario("isaack","Senior",20000);
+        //
+        FuncionarioCollection func = new FuncionarioCollection();
+        
+        func.adicionarFuncionario(f1);
+        func.adicionarFuncionario(f2);
+        func.adicionarFuncionario(f3);
+        System.out.println("Test1");
+        func.pFuncionario();
+        System.out.println("Test2");
+        func.editarFuncionario(0, f2);
+        func.pFuncionario();
+        func.removerFuncionario(0);
+        System.out.println("Test3");
+        func.pFuncionario();
+                        
+    }
+}
