8a2b9fe045a5baf9761551036730fbc0a42bd411
Isaack <isaack.be@hotmail.com>
2023-10-29 00:17:07 -0300

    Adicionando presenters para as views

---
+++ src/main/java/presenter/ListarFuncionarioPresenter.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package presenter;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author isaac
+ */
+public class ListarFuncionarioPresenter extends JFrame {
+    public ListarFuncionarioPresenter(){
+        initComponents();
+    }
+    
+    public void initComponents(){}
+}
