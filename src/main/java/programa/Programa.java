dbab1d41e047dda431565ca3765718c1f13a5c9c
DOUGLAS BASTOS MERENCIO <76476827+Douglasbm040@users.noreply.github.com>
2023-11-02 17:48:25 -0300

    participacao no projeto
    creditos

--- src/main/java/programa/Programa.java
+++ src/main/java/programa/Programa.java
@@ -10,30 +10,10 @@ import presenter.PrincipalPresenter;
 
 /**
  *
- * @author isaac
+ * @author isaac e Douglas
  */
 public class Programa {
     public static void main(String[] args) {
-        
-        /*System.out.println("Testando");
-        //
-        Funcionario f1 = new Funcionario("douglas","estágiario",2000);
-        Funcionario f2 = new Funcionario("thiago","CTO",200000);
-        Funcionario f3 = new Funcionario("isaack","Senior",20000);
-        //
-        FuncionarioCollection func = FuncionarioCollection.getInstance();
-        
-        func.adicionarFuncionario(f1);
-        func.adicionarFuncionario(f2);
-        func.adicionarFuncionario(f3);
-        System.out.println("Test1");
-        func.pFuncionario();
-        System.out.println("Test2");
-        func.editarFuncionario(0, f2);
-        func.pFuncionario();
-        func.removerFuncionario(0);
-        System.out.println("Test3");
-        func.pFuncionario();*/
         PrincipalPresenter principalPresenter = new PrincipalPresenter();
     }
 }
